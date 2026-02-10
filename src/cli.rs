7603f1a3aecc853750486ef1fde9b11b04d22740
wmboeckman <wmboeckman@gmail.com>
2026-02-10 11:51:27 -0600

    updated cli to conform with new return type

--- src/cli.rs
+++ src/cli.rs
@@ -9,26 +9,13 @@ pub fn cli(args: Vec<String>) -> Result<(), &'static str> {
 
     let arg1: &String = &args[1];
 
-    if !is_valid(&arg1) {
-        return Err("Invalid Argument(s)!");
+    let result = math::hex2dec(arg1);
+
+    // for now, just print
+    match result {
+        Ok(n) => println!("{}", n),
+        Err(e) => println!("ERR: {}", e)
     }
 
-    let result = math::hex2dec(&args[1]);
-
-    println!("{}", result); // for now just print
-
     return Ok(());
-}
-
-
-pub fn is_valid(data: &String) -> bool {
-    
-    let valid_chars: String = String::from("0123456789abcdef"); // all valid hexadecimal chars
-
-    return data.chars().all(
-        |c: char| match valid_chars.find(c) {
-            Some(_index) => true,
-            None => false,
-        }
-    );
 }
