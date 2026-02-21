7c7495a5b07eede58833555908791e226709cc25
wmboeckman <wmboeckman@gmail.com>
2026-02-20 22:43:06 -0600

    refactor: error format and function cleanup - updated error formatting (line numbers should be correct now) - moved helper functions to 'util' module

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-use crate::math::{context::*, conversion::*, offset::*};
+use crate::math::offset::*;
 use crate::io::{cli::*, lines_from_file};
-use crate::util::err::*;
+use crate::util::{*, err::*};
 
 use env_logger::Env;
 use log::{debug, error, warn};
@@ -10,7 +10,6 @@ mod math;
 mod util;
 
 fn main() {
-
     let args = parse_cli();
 
     env_logger::Builder::from_env(
@@ -53,32 +52,32 @@ fn main() {
         }
 
         // iter. groups of 2
-        let mut i: usize = 0;
-        while i <= lines.len() / 2 {
+        let mut i: usize = 1;
+        while i < lines.len() / 2 {
             
-            let a = match conv2dec(&util::sanitize_string(&lines[i])) {
+            let a = match conv2dec(&lines[i]) {
                 Ok(i) => i,
                 Err(e) => {
                     if args.fail_fast {
-                        error!("[stdin:{}] Conversion Error: {}", i, e);
+                        error!("[line:{}] Conversion Error: {}", i, e);
                         std::process::exit(1);
                     }
 
-                    warn!("[stdin:{}] Conversion Error: {}", i, e);
+                    warn!("[line:{}] Conversion Error: {}", i, e);
                     
                     i += 2; continue;
                 }
             };
 
-            let b = match conv2dec(&util::sanitize_string(&lines[i+1])) {
+            let b = match conv2dec(&lines[i+1]) {
                 Ok(i) => i,
                 Err(e) => {
                     if args.fail_fast {
-                        error!("[stdin:{}] Conversion Error: {}", i+1, e);
+                        error!("[line:{}] Conversion Error: {}", i+1, e);
                         std::process::exit(1);
                     }
 
-                    warn!("[stdin:{}] Conversion Error: {}", i+1, e);
+                    warn!("[line:{}] Conversion Error: {}", i+1, e);
 
                     i += 2; continue;
                 }
@@ -90,20 +89,20 @@ fn main() {
             i += 2;
         }
     } else {
-        let mut i: usize = 0;
-        for s in lines {
-            match process_line(&s, target_base) {
+        let mut i: usize = 1;
+        for line in lines {
+            match process_line(&line, target_base) {
                 Ok(st) => {
                     result.push_str(&st);
                     result.push('\n');
                 },
                 Err(e) => {
                     if args.fail_fast {
-                        error!("[stdin:{}] Conversion Error: {}", i, e);
+                        error!("[line:{}] Conversion Error: {}", i, e);
                         std::process::exit(1);
                     }
     
-                    warn!("[stdin:{}] Conversion Error: {}", i, e);
+                    warn!("[line:{}] Conversion Error: {}", i, e);
                 }
             }
             i += 1;
@@ -124,45 +123,4 @@ fn process_line(line: &String, target_base: usize) -> Result<String, ConversionE
     };
 
     return conv2base(decimal_val, target_base);
-}
-
-// TODO: Move below functions to 'util' module
-
-fn conv2dec(line: &String) -> Result<usize, ConversionErrors> {
-    let line_sanitized = util::sanitize_string(line);
-    
-    let possible_context = util::discover_base(&line_sanitized);
-
-    let decimal_val = match possible_context {
-        Some(context) => {
-            match base2dec(&line_sanitized, context.base, context.charset, &context.prefix) {
-                Ok(i) => i,
-                Err(e) => return Err(e)
-            }
-        },
-        None => match line_sanitized.parse::<usize>() { // check for base-10 decimal
-            Ok(i) => i,
-            Err(_e) => return Err(ConversionErrors::IntConversionError)
-        }
-    };
-
-    return Ok(decimal_val);
-}
-
-fn conv2base(input: usize, target_base: usize) -> Result<String, ConversionErrors> {
-    let convert_result = match target_base {
-
-        10 => Ok(input.to_string()),
-        
-        n =>  {
-            for context in CONTEXT_REGISTRY.contexts {
-                if context.base == n {
-                    return dec2base(&input, context.base, context.charset, &context.prefix);
-                }
-            }
-            return Err(ConversionErrors::UndefinedBaseContextError);
-        }
-    };
-
-    return convert_result;
 }
--- src/util/mod.rs
+++ src/util/mod.rs
@@ -1,4 +1,4 @@
-use crate::math::context::*;
+use crate::{math::{context::*, conversion::*}, util::err::ConversionErrors};
 
 #[cfg(test)]
 mod tests;
@@ -35,4 +35,43 @@ pub fn discover_base(data: &String) -> Option<&BaseContext<'_>> {
     }
 
     return None;
+}
+
+pub fn conv2dec(line: &String) -> Result<usize, ConversionErrors> {
+    let line_sanitized = sanitize_string(line);
+    
+    let possible_context = discover_base(&line_sanitized);
+
+    let decimal_val = match possible_context {
+        Some(context) => {
+            match base2dec(&line_sanitized, context.base, context.charset, &context.prefix) {
+                Ok(i) => i,
+                Err(e) => return Err(e)
+            }
+        },
+        None => match line_sanitized.parse::<usize>() { // check for base-10 decimal
+            Ok(i) => i,
+            Err(_e) => return Err(ConversionErrors::IntConversionError)
+        }
+    };
+
+    return Ok(decimal_val);
+}
+
+pub fn conv2base(input: usize, target_base: usize) -> Result<String, ConversionErrors> {
+    let convert_result = match target_base {
+
+        10 => Ok(input.to_string()),
+        
+        n =>  {
+            for context in CONTEXT_REGISTRY.contexts {
+                if context.base == n {
+                    return dec2base(&input, context.base, context.charset, &context.prefix);
+                }
+            }
+            return Err(ConversionErrors::UndefinedBaseContextError);
+        }
+    };
+
+    return convert_result;
 }
