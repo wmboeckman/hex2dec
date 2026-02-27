5b14e1a65036e0ac388a920b51a16b67f1b8bbc7
Will Boeckman <wmboeckman@gmail.com>
2026-02-27 15:08:44 -0600

    updated cli args for more clarity

--- src/io/cli.rs
+++ src/io/cli.rs
@@ -1,15 +1,16 @@
 use std::path::PathBuf;
-
 use clap::{Args, Parser};
-
 
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
 pub struct Cli {
+    #[command(flatten)]
+    pub input: InputChoiceGroup,
+
     #[clap(long, short)]
     pub debug: bool,
 
-    #[clap(long("ffast"), short('f'))]
+    #[clap(long("failfast"), short('f'))]
     pub fail_fast: bool,
 
     #[clap(long, short)]
@@ -18,10 +19,7 @@ pub struct Cli {
     #[clap(long, short)]
     pub base: Option<usize>,
 
-    #[command(flatten)]
-    pub input: InputChoiceGroup,
-
-    #[clap(long("out"), short('O'), name = "PATH")]
+    #[clap(long("out"), name = "PATH")]
     pub output_file: Option<std::path::PathBuf>,
 }
 
@@ -31,10 +29,9 @@ pub struct InputChoiceGroup {
     #[arg(name = "input")]
     pub str: Option<Vec<String>>,
 
-    #[arg(short('I'), long("in"))]
+    #[arg(long("in"))]
     pub path: Option<PathBuf>,
 }
-
 
 pub fn parse_cli() -> Cli {
     return Cli::parse();
